8a426a015e031f58582a3da41b66f03261a5cdae
Md Aslam Ali <mdaslamali675@gmail.com>
2022-10-31 18:54:24 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Router/Routes/Routes";
+
+function App() {
+  return (
+    <div data-theme="light" className="max-w-screen-xl mx-auto">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
