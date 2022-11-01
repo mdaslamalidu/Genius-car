3f6995d64437307d021e3ee0d33ba0666652e640
Md Aslam Ali <mdaslamali675@gmail.com>
2022-11-01 10:16:28 +0600

    add firebase

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,18 +1,19 @@
 import React from "react";
+import img from "../../assets/images/login/login.svg";
 
 const Login = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="hero">
-      <div className="hero-content flex-col lg:flex-row">
-        <div className="text-center lg:text-left w-1/2">
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
+      <div className="hero-content flex-col lg:flex-row gap-12 my-9">
+        <div className="text-center lg:text-left w-1/2 py-5">
+          <img className="w-3/4" src={img} alt="" />
         </div>
         <div className="card flex-shrink-0 max-w-sm shadow-2xl bg-base-100 w-1/2">
-          <div className="card-body">
+          <form onSubmit={handleSubmit} className="card-body">
             <h1 className="text-5xl font-bold">Login now!</h1>
             <div className="form-control">
               <label className="label">
@@ -40,9 +41,9 @@ const Login = () => {
               </label>
             </div>
             <div className="form-control mt-6">
-              <button className="btn btn-primary">Login</button>
+              <input className="btn btn-primary" type="submit" value="Login" />
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
