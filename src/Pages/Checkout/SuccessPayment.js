a47190a01a0898b18b6def664d402e2b1d530b7b
mdaslamalidu <thehometutor2022@gmail.com>
2023-01-12 01:01:55 +0600

    add table for data

--- src/Pages/Checkout/SuccessPayment.js
+++ src/Pages/Checkout/SuccessPayment.js
@@ -1,12 +1,54 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { useLocation } from "react-router-dom";
 
 const SuccessPayment = () => {
+  const [order, setOrder] = useState({});
   const location = useLocation();
   const query = new URLSearchParams(location.search);
   const transactionId = query.get("transactionId");
   console.log(transactionId);
-  return <div></div>;
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/order/by_paymentId/${transactionId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setOrder(data);
+      });
+  }, [transactionId]);
+  return (
+    <div>
+      <h2>Successfully paid Congrats!!</h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Shipping Address</th>
+              <th>Transaction Id</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>{order.serviceName}</th>
+              <td>{order.price}</td>
+              <td>{order.address}</td>
+              <td>{order.transactionId}</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <button
+          className="btn btn-primary ml-auto mt-5 block"
+          onClick={() => window.print()}
+        >
+          Print
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default SuccessPayment;
